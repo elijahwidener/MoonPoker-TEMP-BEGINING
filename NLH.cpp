4454df2826e2caeb922e51b844f15c75220adb3a
Elijah Widener Ferreira <elijah.widenerferreira@gmail.com>
2023-08-15 09:50:51 -0400

    add bet to player, began playerActions function

--- NLH.cpp
+++ NLH.cpp
@@ -1,26 +1,28 @@
 #include "player\Player.h"
 
 
-void playGame(Player&, vector<Player>&, vector<Card>&, Deck&, int);
+void playGame(Player&, vector<Player>&, vector<Player>&, vector<Card>&, Deck&, int);
 
 main(){
 
     Deck deck;
     int NLHNumCards = 2;
+    int zeroBet = 0;
     std::vector<Player> players;
+    std::vector<Player> inHand;
     std::vector<Card> muckedCards;
 
 
     //Create the dealer, which will also be the board
-    Player dealer(deck, 0);
+    Player dealer(deck, 0, 0);
 
     //create 6 players with empty hands
     for(int i = 0; i < 6; i++) {
-        players.push_back(Player(deck, 0));
+        players.push_back(Player(deck, 0, 0));
     }
 
     for (int k = 0; k < 3; k++){
-        playGame(dealer, players, muckedCards, deck, NLHNumCards);
+        playGame(dealer, players, inHand, muckedCards, deck, NLHNumCards);
     }
 
     return 0;
@@ -28,7 +30,8 @@ main(){
 }
 
 
-void playGame(Player& dealer, vector<Player>& players, vector<Card>& muckedCards, Deck& deck, int NLHNumCards){
+void playGame(Player& dealer, vector<Player>& players, vector<Player>& inHand, vector<Card>& muckedCards, 
+        Deck& deck, int NLHNumCards){
 
     
 
@@ -39,7 +42,8 @@ void playGame(Player& dealer, vector<Player>& players, vector<Card>& muckedCards
 
     //deal all cards
     for (int i = 0; i< 6; i++){
-        players[i].dealCards(NLHNumCards);
+        players[i].dealCards(NLHNumCards); //give the player cards
+        inHand.push_back(players[i]);      //put the player in the hand
     }
     dealer.dealCards(5);
 
@@ -58,5 +62,66 @@ void playGame(Player& dealer, vector<Player>& players, vector<Card>& muckedCards
     }
 }
 
+
+void playerActions(vector<Player>& inHand, vector<Card>& muckedCards, int bet, int NLHNumCards){
+    int currentBet;
+    
+    auto actionsOnYou = inHand.begin();
+
+    while(actionsOnYou != inHand.end()){
+        actionsOnYou->setBet(actionsOnYou->action(muckedCards, bet, NLHNumCards));
+        if(actionsOnYou->getBet() == -1){
+            //Remove the player from in the hand
+            inHand.erase(actionsOnYou);
+        }
+        else{
+            currentBet = actionsOnYou->getBet();
+            ++actionsOnYou;
+            //Add bet to pot
+        }
+    }  
+        
+    while(!allBetsEqual(inHand, currentBet)){
+        while(actionsOnYou != inHand.end()){
+            actionsOnYou->setBet(actionsOnYou->action(muckedCards, bet, NLHNumCards));
+            if(actionsOnYou->getBet() == -1){
+                //Remove the player from in the hand
+                inHand.erase(actionsOnYou);
+            }
+            else{
+                currentBet = actionsOnYou->getBet();
+                ++actionsOnYou;
+                //Add bet to pot
+            }
+        }
+    }
+
+    //create a function that checks if all bets are equal
+
+    //while allBetsEqual is false, ask the players in the hand who dont equal the current bet again
+}
+
+
+bool allBetsEqual(vector<Player> inHand, int currentBet){
+    for (Player player : inHand){
+        if(player.getBet() != currentBet){
+            return false;
+        }
+    }
+    return true;
+}
+
+
+/*void takeAction(vector<Player>& players, vector<Card>& muckedCards, int bet, int NLHNumCards){
+    player.setBet(player.action(muckedCards, bet, NLHNumCards));
+        if(player.getBet() == -1){
+            //Remove the player from in the hand
+            inHand.pop(player);
+        }
+        else{
+            currentBet = player.getBet();
+            //Add bet to pot
+        }
+}*/
 
 
--- player/Player.h
+++ player/Player.h
@@ -8,8 +8,8 @@ class Player{
         //Create the player Constructor. Calls a deck and a desired number of cards. 
                 //In the future will potentially need to call more, like a pot size, or stack size
                 //Will also need a position to be held by each player, and be able to rotate position
-        Player(Deck& deck, int desiredNumCards) : deck(deck) {
-            dealCards(desiredNumCards);
+        Player(Deck& deck, int numberOfCards, int bet) : deck(deck) {
+            dealCards(numberOfCards);
         }
 
         //Deals a player two distinct random cards using the Deck class
@@ -19,13 +19,14 @@ class Player{
             }
         };
 
+        //mainly for testing, shows the cards
         void showCards(int numberOfCards){
             for(int i= 0; i < numberOfCards; i++ ){
                 cout << hand[i] << ",";
             }
             cout << endl;
         }
-
+        //puts cards into the muck pile, and removes them from the hand. 
         void muckCards(int numberOfCards, vector<Card>& muckedCards){
             
             //Add the mucked card to the mucked card vector, then remove it form the hand
@@ -37,9 +38,68 @@ class Player{
 
         
 
+        int action(vector<Card>& muckedCards, int bet, int numberOfCards){
+            
+            int raiseAmount = 0;
+            char playerAction;
+
+            if (bet == 0){
+                cout << "'x' to check " << endl;
+            }
+            if (bet != 0){
+                cout << "'c' to call " << endl;
+            }
+            cout << "'r' to raise " << endl;
+            cout << "'f' to fold " << endl;
+            cout << endl;
+            cin >> playerAction;
+
+
+        switch(playerAction) {
+            case 'x': 
+                return 0;
+                break;
+
+            case 'c': 
+                return bet;
+                break;
+
+            case 'r':
+                do {
+                    cout << "Enter raise amount (must be at least double the bet): ";
+                    cin >> raiseAmount;
+                    if (raiseAmount < 2 * bet) {
+                        cout << "Raise must be double the bet or more. Please try again." << endl;
+                    }
+                } while (raiseAmount < 2 * bet);
+                return raiseAmount;
+                break;
+
+            case 'f':
+                muckCards(numberOfCards, muckedCards);
+                return -1;
+
+            default:
+                cout << "Invalid input! Please enter a valid action." << endl;
+                return action(muckedCards, bet, numberOfCards); // Recursive call
+                break;
+        }
+    }
+
+    //Set bet
+    void setBet(int newBet) {
+        bet = newBet;
+    }
+    //Get Bet
+    int getBet(){
+        return bet;
+    }
+        
+
 
     private:
-        int desiredNumCards;
+        int bet;
+        int numberOfCards;
         Deck& deck;
         std::vector<Card> hand;
         std::vector<Card> muckedCards;
